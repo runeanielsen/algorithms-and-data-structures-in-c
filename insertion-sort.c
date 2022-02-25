b0454aab48c2fc0dfc69d92347f080d45df8e453
Rune Nielsen <runenielsen@runbox.com>
2022-02-25 20:39:57 +0100

    now supports both signed and unsigned types

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,30 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_array(int n[], int l) {
-  for (int i = 0; i < l; i++) {
-    printf("%d\n", n[i]);
+void print_array(size_t n[], size_t l) {
+  for (size_t i = 0; i < l; i++) {
+    printf("%zu\n", n[i]);
   }
 }
 
-void insertion_sort(int arr[], int n) {
-  for (int i = 1; i < n; i++)
+void insertion_sort(size_t arr[], size_t n) {
+  for (size_t i = 1; i < n; i++)
   {
-    int tmp = arr[i];
-    int j = i - 1;
-
-    while (j >= 0 && arr[j] > tmp)
+    size_t tmp = arr[i];
+    size_t j = i;
+    while (j > 0 && arr[j - 1] > tmp)
     {
-      arr[j + 1] = arr[j];
-      j = j - 1;
+      arr[j] = arr[j - 1];
+      j--;
     }
-    arr[j + 1] = tmp;
+    arr[j] = tmp;
   }
 }
 
 int main(void) {
-  int n[] = { 4, 2, 7, 1, 3 };
-  int l = sizeof(n) / sizeof(*n);
+  size_t n[] = { 4, 2, 7, 1, 3 };
+  size_t l = sizeof(n) / sizeof(*n);
 
   insertion_sort(n, l);
   print_array(n, l);
