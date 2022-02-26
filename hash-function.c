4ab0aebfa820a3affdfa01926930d5f83b485080
Rune Nielsen <runenielsen@runbox.com>
2022-02-26 12:07:21 +0100

    adds example of simple hash function

---
+++ hash-function.c
@@ -0,0 +1,19 @@
+/* Example of a simple hash function */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+size_t hash(char* string, size_t length) {
+  size_t sum = 0;
+  for (size_t i = 0; i < length; i++) {
+    sum += (size_t)string[i];
+  }
+  return sum - (length * 64);
+}
+
+int main(void) {
+  char* string = "BAD";
+  size_t length = strlen(string);
+  size_t hash_value = hash(string, length);
+  printf("The hash of %s is %zu\n", string, hash_value);
+}
