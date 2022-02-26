aa540ff06dd53bcd6db54d8a3559b6929a5e50ad
Rune Nielsen <runenielsen@runbox.com>
2022-02-26 14:29:02 +0100

    switch to multiplication hash function

--- hash-function.c
+++ hash-function.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 size_t hash(char* string, size_t length) {
-  size_t sum = 0;
+  size_t sum = 1;
   for (size_t i = 0; i < length; i++) {
-    sum += (size_t)string[i];
+    sum *= (size_t)string[i] - 64;
   }
-  return sum - (length * 64);
+  return sum;
 }
 
 int main(void) {
---
+++ hash-table.c
@@ -0,0 +1,10 @@
+/* Example of a simple hash-table implementation */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+
+
+  return EXIT_SUCCESS;
+}
