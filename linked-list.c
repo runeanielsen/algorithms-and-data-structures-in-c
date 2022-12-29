292ccf0e3ae4af9c63894524e414691da0808563
Rune Nielsen <runenielsen@runbox.com>
2022-12-29 21:45:30 +0100

    now uses struct LinkedList to manage the linked list

--- linked-list.c
+++ linked-list.c
@@ -1,47 +1,62 @@
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node {
   size_t value;
-  struct Node* next;
+  struct Node *next;
 } Node;
 
-Node* create_node(size_t value) {
-  Node* node = malloc(sizeof(Node));
-  node->value = value;
-  return node;
+typedef struct LinkedList {
+  Node *start;
+  Node *end;
+} LinkedList;
+
+static void init_linked_list(LinkedList *linked_list, Node *node) {
+  linked_list->end = node;
+  linked_list->start = node;
 }
 
-Node* add_node(size_t value, Node* head) {
-  Node* node = create_node(value);
-  node->next = head;
-  return node;
+static Node *new_node(size_t value) {
+  Node *n = malloc(sizeof(Node));
+  n->value = value;
+  n->next = NULL;
+  return n;
 }
 
-void print_nodes(Node* head) {
+static void print_nodes(const LinkedList *linked_list) {
+  Node *head = linked_list->start;
   while (head) {
-    printf("Node has value: %zu\n", head->value);
+    printf("Node value is: %zu\n", head->value);
     head = head->next;
   }
 }
 
-void free_nodes(Node* head) {
+static void append_node(LinkedList *linked_list, Node *next) {
+  linked_list->end->next = next;
+  linked_list->end = next;
+}
+
+static void free_nodes(LinkedList *linked_list) {
+  Node *head = linked_list->start;
   while (head) {
-    Node* tmp = head->next;
+    Node *tmp = head->next;
     free(head);
     head = tmp;
   }
 }
 
-int main(void) {
-  Node* head = create_node(0);
-  for (size_t i = 1; i < 10; i++) {
-    head = add_node(i, head);
+int main() {
+  LinkedList my_linked_list;
+  init_linked_list(&my_linked_list, new_node(0));
+
+  for (size_t i = 1; i < 100; i++) {
+    append_node(&my_linked_list, new_node(i * 100));
   }
 
-  print_nodes(head);
-  free_nodes(head);
-  head = NULL;
+  print_nodes(&my_linked_list);
+
+  free_nodes(&my_linked_list);
 
   return EXIT_SUCCESS;
 }
