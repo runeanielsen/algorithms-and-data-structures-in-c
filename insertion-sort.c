987874649dc3dea87e896f393555087994579333
Rune Nielsen <runenielsen@runbox.com>
2023-01-01 18:51:38 +0100

    fix formatting

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_array(size_t n[], size_t l) {
   for (size_t i = 0; i < l; i++) {
@@ -8,12 +8,10 @@ void print_array(size_t n[], size_t l) {
 }
 
 void insertion_sort(size_t arr[], size_t n) {
-  for (size_t i = 1; i < n; i++)
-  {
+  for (size_t i = 1; i < n; i++) {
     size_t tmp = arr[i];
     size_t j = i;
-    while (j > 0 && arr[j - 1] > tmp)
-    {
+    while (j > 0 && arr[j - 1] > tmp) {
       arr[j] = arr[j - 1];
       j--;
     }
@@ -22,7 +20,7 @@ void insertion_sort(size_t arr[], size_t n) {
 }
 
 int main(void) {
-  size_t n[] = { 4, 2, 7, 1, 3 };
+  size_t n[] = {4, 2, 7, 1, 3};
   size_t l = sizeof(n) / sizeof(*n);
 
   insertion_sort(n, l);
