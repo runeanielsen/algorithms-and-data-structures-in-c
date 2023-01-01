32a3362e41f55c199056daff52d553a6574677c5
Rune Nielsen <runenielsen@runbox.com>
2023-01-01 18:52:34 +0100

    switch to static, they're not used externally

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_array(size_t n[], size_t l) {
+static void print_array(size_t n[], size_t l) {
   for (size_t i = 0; i < l; i++) {
     printf("%zu\n", n[i]);
   }
 }
 
-void insertion_sort(size_t arr[], size_t n) {
+static void insertion_sort(size_t arr[], size_t n) {
   for (size_t i = 1; i < n; i++) {
     size_t tmp = arr[i];
     size_t j = i;
