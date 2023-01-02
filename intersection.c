74511bedcf7e6e3c650ad19243f8f7e644e2fdae
Rune Nielsen <runenielsen@runbox.com>
2023-01-02 11:50:50 +0100

    standard formatting

--- intersection.c
+++ intersection.c
@@ -1,19 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct  {
+typedef struct {
   size_t length;
-  size_t* values;
+  size_t *values;
 } Array;
 
-void print_array(Array* a) {
+void print_array(Array *a) {
   for (size_t i = 0; i < a->length; i++) {
     printf("%zu\n", a->values[i]);
   }
 }
 
-Array* intersection(Array* a1, Array* a2) {
-  Array* result = malloc(sizeof(Array));
+Array *intersection(Array *a1, Array *a2) {
+  Array *result = malloc(sizeof(Array));
   result->values = malloc(0);
   result->length = 0;
 
@@ -21,7 +21,7 @@ Array* intersection(Array* a1, Array* a2) {
     for (size_t j = 0; j < a2->length; j++) {
       if (a1->values[i] == a2->values[j]) {
         result->length++;
-        size_t* tmp = realloc(result->values, result->length * sizeof(size_t));
+        size_t *tmp = realloc(result->values, result->length * sizeof(size_t));
         if (tmp) {
           result->values[result->length - 1] = a1->values[i];
           break;
@@ -36,13 +36,13 @@ Array* intersection(Array* a1, Array* a2) {
   return result;
 }
 
-void free_array(Array* a) {
+void free_array(Array *a) {
   free(a->values);
   free(a);
 }
 
 int main(void) {
-  Array* a1 = malloc(sizeof(Array));
+  Array *a1 = malloc(sizeof(Array));
   a1->length = 4;
   a1->values = malloc(a1->length * sizeof(size_t));
   a1->values[0] = 10;
@@ -50,7 +50,7 @@ int main(void) {
   a1->values[2] = 2;
   a1->values[3] = 25;
 
-  Array* a2 = malloc(sizeof(Array));
+  Array *a2 = malloc(sizeof(Array));
   a2->length = 4;
   a2->values = malloc(a2->length * sizeof(size_t));
   a2->values[0] = 3;
@@ -58,7 +58,7 @@ int main(void) {
   a2->values[2] = 10;
   a2->values[3] = 5;
 
-  Array* intersections = intersection(a1, a2);
+  Array *intersections = intersection(a1, a2);
   print_array(intersections);
 
   free_array(intersections);
